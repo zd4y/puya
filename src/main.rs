84a67898d67306311acb1fff7b33b61d9103752d
zd4y <59004538+zd4y@users.noreply.github.com>
2021-11-03 17:40:17 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use reqwest::blocking::{self, Response};
+use rss::Channel;
+use std::process::Command;
+use std::{env::args, error::Error, io::Write, process::Stdio};
+
+fn fetch_data() -> Response {
+    blocking::get("https://nyaa.si/?page=rss&u=puyero").expect("Error fetching data")
+}
+
+#[derive(Debug, Clone)]
+pub struct Anime {
+    title: String,
+    link: String,
+}
+
+fn get_animes() -> Result<Vec<Anime>, Box<dyn Error>> {
+    let res = fetch_data();
+    let channel = Channel::read_from(&res.bytes()?[..])?;
+
+    let mut animes = Vec::new();
+
+    for item in channel.items() {
+        let title = item.title.as_ref().expect("item missing title").to_string();
+        let link = item.link.as_ref().expect("item missing link").to_string();
+        let anime = Anime { title, link };
+        animes.push(anime);
+    }
+
+    Ok(animes)
+}
+
+fn get_command() -> Command {
+    let mut args = args();
+    let program = args.nth(1).expect("missing program");
+
+    let mut command = Command::new(program);
+    command.args(args);
+    command.stdin(Stdio::piped());
+    command.stdout(Stdio::piped());
+    command
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let animes = get_animes()?;
+
+    let mut child = get_command().spawn().expect("error executing command");
+
+    let mut stdin = child.stdin.take().expect("Failed to open stdin");
+    let a = animes.clone();
+    std::thread::spawn(move || {
+        let x = a
+            .iter()
+            .map(|a| a.title.clone())
+            .collect::<Vec<String>>()
+            .join("\n");
+        stdin
+            .write_all(x.as_bytes())
+            .expect("Failed to write to stdin");
+    });
+
+    let output = child.wait_with_output().expect("Failed to read stdout");
+    let output = String::from_utf8_lossy(&output.stdout);
+    let output = output.trim();
+
+    let anime = animes.iter().find(|a| a.title == output).expect("anime not found");
+
+    println!("{}", anime.link);
+
+    Ok(())
+}
