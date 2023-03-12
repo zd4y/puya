f7a1bf8bb4de536b52b2f342de1a41d004051dd6
zd4y <59004538+zd4y@users.noreply.github.com>
2023-03-12 17:30:07 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use inquire::Select;
 use reqwest::blocking::{self, Response};
 use rss::Channel;
-use std::error::Error;
+use std::{error::Error, fmt};
 
 fn fetch_data() -> Response {
     blocking::get("https://nyaa.si/?page=rss&u=puyero").expect("Error fetching data")
@@ -29,63 +29,21 @@ fn get_animes() -> Result<Vec<Anime>, Box<dyn Error>> {
     Ok(animes)
 }
 
-/* fn get_command() -> Command {
-    let mut args = args();
-    let program = args.nth(1).expect("missing program");
-
-    let mut command = Command::new(program);
-    command.args(args);
-    command.stdin(Stdio::piped());
-    command.stdout(Stdio::piped());
-    command
-} */
-
 fn main() -> Result<(), Box<dyn Error>> {
     let animes = get_animes()?;
-    let items: Vec<_> = animes
-        .iter()
-        .map(|anime| &anime.title)
-        .map(|title| {
-            title
-                .trim_start_matches("[PuyaSubs!] ")
-                .trim_end_matches(".mkv")
-                .replace("[ESP-ENG]", "")
-        })
-        .collect();
-
-    let selection = Select::new("Choose the anime", items).raw_prompt()?;
-
-    /* let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-    .items(&items)
-    .default(0)
-    .interact()?; */
-
-    let selected_anime = &animes[selection.index];
-
+    let selected_anime = Select::new("Choose the anime", animes).prompt()?;
     println!("{}", selected_anime.link);
 
-    /* let mut child = get_command().spawn().expect("error executing command");
-
-    let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    let a = animes.clone();
-    std::thread::spawn(move || {
-        let x = a
-            .iter()
-            .map(|a| a.title.clone())
-            .collect::<Vec<String>>()
-            .join("\n");
-        stdin
-            .write_all(x.as_bytes())
-            .expect("Failed to write to stdin");
-    });
-
-    let output = child.wait_with_output().expect("Failed to read stdout");
-    let output = String::from_utf8_lossy(&output.stdout);
-    let output = output.trim();
-
-    let anime = animes.iter().find(|a| a.title == output).expect("anime not found");
-
-    println!("{}", anime.link); */
-
     Ok(())
+}
+
+impl fmt::Display for Anime {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+        let title = self
+            .title
+            .trim_start_matches("[PuyaSubs!] ")
+            .trim_end_matches(".mkv")
+            .replace("[ESP-ENG]", "");
+        write!(f, "{}", title)
+    }
 }
